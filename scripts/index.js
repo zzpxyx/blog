903da9e971e351ccb40cbaf28276b89c0e21d066
zzpxyx <zptomz@gmail.com>
2016-02-05 23:24:28 -0600

    Improve script.

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,16 @@ function initOnLoad() {
     document.addEventListener('DOMContentLoaded', function() {
         var anchorList = document.getElementsByTagName('a');
         var lastAnchor = anchorList[anchorList.length - 1];
-        var URL = lastAnchor.href;
-        var iframe = document.createElement('iframe');
-        iframe.width = '700px';
-        iframe.height = '350px';
-        iframe.name = 'show_email';
-        iframe.style = 'display: none;';
-        document.body.appendChild(iframe);
-        lastAnchor.target = iframe.name;
         lastAnchor.addEventListener('click', function() {
-            iframe.style = 'border: 1px solid #808080; display: block;';
+            if (document.getElementsByName('show_email').length == 0) {
+                var iframe = document.createElement('iframe');
+                iframe.width = '700px';
+                iframe.height = '350px';
+                iframe.name = 'show_email';
+                iframe.style = 'border: 1px solid #808080; display: block;';
+                lastAnchor.target = iframe.name;
+                document.body.appendChild(iframe);
+            }
         });
     })
 }

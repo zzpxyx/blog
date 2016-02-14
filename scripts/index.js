d7e78eeb1a3ee16c9efd7882091dd85c6b581453
zzpxyx <zptomz@gmail.com>
2016-02-13 23:45:53 -0600

    Add experimental scrolling to iframe.

--- scripts/index.js
+++ scripts/index.js
@@ -9,8 +9,26 @@ function initOnLoad() {
                 iframe.height = '350px';
                 iframe.name = 'show_email';
                 iframe.style = 'border: 1px solid #808080; display: block;';
-                lastAnchor.target = iframe.name;
+                iframe.addEventListener('load', function() {
+                    var html = document.documentElement;
+                    var body = document.body;
+                    var distance = 400;
+                    var stride;
+                    window.setTimeout(function scroll() {
+                        if (distance > 0) {
+                            stride = 100 * (distance / 400) * (distance / 400);
+                            if (stride < 1) {
+                                stride = 1;
+                            }
+                            html.scrollTop += stride;
+                            body.scrollTop += stride;
+                            distance -= stride;
+                            window.setTimeout(scroll, 20);
+                        }
+                    }, 20);
+                });
                 document.body.appendChild(iframe);
+                lastAnchor.target = iframe.name;
             }
         });
     })
